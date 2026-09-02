8f1844d6ae842144f4df51106a92acb8c92cabe1
AlgrdFit <Algirdas.Fitkevicius@gmail.com>
2026-09-02 16:58:03 +0300

    KAN-7: Add login presentation

---
+++ feature/login/impl/src/main/java/com/deeper/deepertask/feature/login/impl/presentation/LoginUiState.kt
@@ -0,0 +1,21 @@
+package com.deeper.deepertask.feature.login.impl.presentation
+
+import com.deeper.deepertask.feature.login.impl.domain.model.LoginError
+
+internal data class LoginUiState(
+    val email: String = "",
+    val password: String = "",
+    val isEmailMissing: Boolean = false,
+    val isPasswordMissing: Boolean = false,
+    val status: LoginStatus = LoginStatus.Idle,
+)
+
+internal sealed interface LoginStatus {
+    data object Idle : LoginStatus
+
+    data object Loading : LoginStatus
+
+    data object Success : LoginStatus
+
+    data class Error(val error: LoginError) : LoginStatus
+}
