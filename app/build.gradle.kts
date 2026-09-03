a1413efb946aff9178f1288e893f69c9ce24b8ae
AlgrdFit <Algirdas.Fitkevicius@gmail.com>
2026-09-03 10:31:18 +0300

    KAN-9: Add bathymetry modules and Maps setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("deepertask.android.application")
     id("deepertask.android.compose")
     id("deepertask.android.hilt")
+    alias(libs.plugins.secrets.gradle.plugin)
 }
 
 android {
@@ -12,6 +13,8 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    buildFeatures.buildConfig = true
 
     buildTypes {
         release {
@@ -26,11 +29,17 @@ dependencies {
     implementation(projects.core.designsystem)
     implementation(projects.core.navigation)
     implementation(projects.feature.login.impl)
+    implementation(projects.feature.bathymetry.impl)
     implementation(projects.feature.scans.impl)
 
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
+}
+
+secrets {
+    defaultPropertiesFileName = "local.defaults.properties"
 }
